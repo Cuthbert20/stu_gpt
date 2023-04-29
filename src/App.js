0c11ceaaf29cac19c370b4e963540fd7fa23e3b1
Spencer Knowlton <knowltonspencer@gmail.com>
2023-04-29 13:54:10 -0600

    setting up input

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ const App = () => {
           <section className="side-bar">
         <button>+ New Chat</button>
         <ul className="history">
-          <li>foo</li>
-          <li>bar</li>
         </ul>
         <nav>
           <p>Built by Stu</p>
         </nav>
     </section>
     <section className="main">
-        
+        <h1>STU's GPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit" >✒</div>
+          </div>
+        </div>
         <p className="info">Chat GPT Mar 23 Version.</p>
     </section>
     </div>  
