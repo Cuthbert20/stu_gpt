f6d8824f749ef51918d1d443262068ebdac4b93a
Spencer Knowlton <knowltonspencer@gmail.com>
2023-04-30 09:12:37 -0600

    setting up server to make fetch rquest

--- server.js
+++ server.js
@@ -2,8 +2,43 @@ const PORT = 8000;
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 //Middleware
+//If we want to use JSON in our application, we need to use the express.json() middleware.
 app.use(express.json());
 app.use(cors());
 
+const API_KEY = process.env.API_KEY;
+
+app.post('/completion', async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": req.body.message}],
+            max_tokens: 20,
+        })
+    };
+    try {
+       const results = await fetch("https://api.openai.com/v1/chat/completions", options)
+        const data = await results.json();
+        res.json(data);
+    }catch (err) {
+        console.log(`Error: ${err.message}`);
+    }
+});
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const createNewChat = () => {
+    setCurrentTitle(null);
+    setValue(null);
+    setMessage(null);
+  }
+
+  const getMessage = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        //Value is being set by the input field.
+        message: value
+      })
+    };
+    try {
+      const response = await fetch('http://localhost:8000/completion', options);
+      const data = await response.json();
+      console.log(data);
+      //message is an obj role & content.
+      setMessage(data.choices[0].message);
+    } catch(err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((previousChats) => [
+        ...previousChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content: value,
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
+    }
+  }, [message, currentTitle]);
+
+  console.log(previousChats);
+
+  const currentChats = previousChats.filter(previousChat => previousChat.title === currentTitle);
+
   return (
     <div className="app">
           <section className="side-bar">
-        <button className="chat-button">+ New Chat</button>
+        <button onClick={createNewChat} className="chat-button">+ New Chat</button>
         <ul className="history">
           <li className="active">Chat 1</li>
         </ul>
@@ -13,14 +72,14 @@ const App = () => {
         </nav>
     </section>
     <section className="main">
-        <h1>STU's GPT</h1>
+        {!currentTitle && <h1>STU's GPT</h1>}
         <ul className="feed">
 
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input/>
-            <div id="submit" >✒</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessage}>✒</div>
           </div>
           <p className="info">Chat GPT Mar 23 Version.</p>
         </div>
